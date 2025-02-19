db59d478fbb1e8ad108dc401b726f8cb09a4e8e8
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-19 12:33:37 +0530

    [Abhishek] add. JSON Creating JSON object for a Student Solution

---
+++ JSON/src/test/java/com/practiceproblems/json/CreateJsonObjectTest.java
@@ -0,0 +1,12 @@
+package com.practiceproblems.json;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateJsonObjectTest {
+
+    @Test
+    void testJsonObjectCreation() {
+      String ans="{\"subject\":[\"Java\",\"Hindi\",\"Science\"],\"name\":\"Alice\",\"age\":25}";
+      assertEquals(CreateJsonObject.createJsonObject(),ans);
+    }
+}
