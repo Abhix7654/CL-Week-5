39dedb8a3c737d037351f5e95e853146a30cd841
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-19 16:49:00 +0530

    [Abhishek] add. JSON Filter JSON data: Print only users older than 25 years Solution

---
+++ JSON/src/main/java/com/handsonpracticeproblems/json/FilterJsonByAge.java
@@ -0,0 +1,24 @@
+package com.handsonpracticeproblems.json;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class FilterJsonByAge {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File("src/main/resources/people.json"));
+
+            List<JsonNode> filteredRecords = new ArrayList<>();
+            for (JsonNode node : rootNode) {
+                    if (node.has("age") && node.get("age").asInt() > 25) {
+                        filteredRecords.add(node);
+                    }
+                }
+
+            System.out.println(filteredRecords);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
