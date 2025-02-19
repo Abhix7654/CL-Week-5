16d1b62feacf2868601407927c8689c3ebe57330
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-19 13:15:19 +0530

    [Abhishek] add. JSON Read a JSON file and extract only specific fields Solution

---
+++ JSON/src/test/java/com/practiceproblems/json/ReadJsonExtractFieldsTest.java
@@ -0,0 +1,26 @@
+package com.practiceproblems.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import java.io.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadJsonExtractFieldsTest {
+    @Test
+    void readData(){
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File("src/main/resources/output.json"));
+            String brand = rootNode.get("brand").asText();
+            String model = rootNode.get("model").asText();
+
+            assertEquals("BMW",brand);
+            assertEquals("M-5",model);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
